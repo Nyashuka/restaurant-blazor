450cf9178fbb740f10ba2d710029b709e83ab577
Nyashuka <godnebeles@gmail.com>
2024-11-21 14:00:27 +0200

    init project

---
+++ RestaurantApp/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddLibraries(
+        this IServiceCollection services
+    )
+    {
+
+        return services;
+    }
+}
